80280416514be3abfa9d34c9816e52dedd00e7b7
sivsais <sivsais@gmail.com>
2015-09-15 22:32:40 +0300

    feat(main): synchronizer

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require.config({
-    baseUrl: './js',
+    baseUrl: '.',
     paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
-        text: '../bower_components/text/text',
-        paper: '../bower_components/paper/dist/paper-full.min'
+        jquery: 'bower_components/jquery/dist/jquery',
+        text: 'bower_components/text/text',
+        paper: 'bower_components/paper/dist/paper-full.min'
     },
     shim: {
         paper: {
@@ -13,28 +13,28 @@ require.config({
     packages: [
         {
             name: 'dat',
-            location: '../bower_components/dat-gui/src/dat',
+            location: 'bower_components/dat-gui/src/dat',
             main: 'gui/GUI'
         }, {
             name: 'mg-gui',
-            location: '../../mg-gui/src',
+            location: 'bower_components/mg-gui/src',
             main: 'mg-gui'
         }, {
             name: 'mg-sheet',
-            location: '../../mg-sheet/js/mg-sheet',
-            main: 'mg-sheet'
+            location: 'bower_components/mg-sheet/src/mg-sheet',
+            main: 'sheet/sheet'
         }, {
             name: 'mg-space2',
-            location: '../../mg-space2/js/mg-space2',
-            main: 'mg-space2'
+            location: 'bower_components/mg-space2/src/mg-space2',
+            main: 'space2/space2'
         }
     ]
 });
 
-define(['mg-gui', 'mg-sheet', 'mg-space2'], function (gui, Sheet, space2) {
+define(['mg-gui', 'mg-sheet', 'mg-space2', './js/synchronizer'], function (gui, Sheet, space2, synch) {
     var sheet = new Sheet('canvas');
     var axes0 = space2.make_axes('affine');
-    axes0.basis = [[1, 0], [-1, 0]];
+    axes0.basis = [[1, 0], [0, -1]];
 
     var generate_additional_segment = function (project) {
         return {
@@ -43,19 +43,15 @@ define(['mg-gui', 'mg-sheet', 'mg-space2'], function (gui, Sheet, space2) {
                 y: 400
             },
             fields: [
-                { name:'point2--x',
-                    type:'slider',
-                    value: project.point2.x,
-                    change: function(v) {
-                        project.point2.x = v;
-                    }
+                {
+                    name: 'point2--x',
+                    type: 'slider',
+                    value: project.point2.x
                 },
-                { name:'point2--y',
-                    type:'slider',
-                    value: project.point2.y,
-                    change: function(v) {
-                        project.point2.y = v;
-                    }
+                {
+                    name: 'point2--y',
+                    type: 'slider',
+                    value: project.point2.y
                 }
             ]
         }
@@ -65,27 +61,19 @@ define(['mg-gui', 'mg-sheet', 'mg-space2'], function (gui, Sheet, space2) {
         name: 'arrow',
         target: 'sheet',
         mode: 'single',
-        mouseDown: function (sheet, event) {},
+        mouseDown: function (sheet, event) {
+        },
         mouseDrag: function (sheet, event) {
             if (!this._current) {
 
                 this._model = space2.make_segment().make_project(axes0);
-                this._current = sheet.draw_arrow(event.point, event.point);
-                var self = this;
-                this._current.on('change', function () {
-                    console.log(self._current.to.x);
-                    self._model.point1.x = self._current.from.x;
-                    self._model.point1.x = self._current.from.y;
-                    self._model.point2.x = self._current.to.x;
-                    self._model.point2.y = self._current.to.y;
-                    console.log(self._model.point2.x);
-                    self._add.fields[0].value = self._model.point2.x;
-                    self._add.fields[1].value = self._model.point2.y;
-                });
-                this._add = generate_additional_segment(self._model);
+                this._model.point1 = event.point;
+                this._current = sheet.draw_segment(event.point, event.point);
+                this._add = generate_additional_segment(this._model);
+                synch(this._model, this._current, this._add);
                 gui.additional.create(this._add);
             }
-            this._current.to = event.point;
+            this._model.point2 = event.point;
         },
         mouseUp: function (sheet, event) {
             this._current = null;
@@ -113,10 +101,10 @@ define(['mg-gui', 'mg-sheet', 'mg-space2'], function (gui, Sheet, space2) {
             id: 0,
             type: 'selectable',
             icon: 'http://dummyimage.com/50x50/ad6685/0c00f0.png&text=arrow',
-            callback: function () {
+            select: function () {
                 arrow_tool.enabled = true;
             },
-            callback_disable: function () {
+            unselect: function () {
                 arrow_tool.enabled = false;
             }
         }, {
---
+++ js/synchronizer.js
@@ -0,0 +1,41 @@
+define([], function () {
+    function logger(m, v, a) {
+        console.log('=====model=====');
+        console.log(m.point1.x, m.point1.y, m.point2.x, m.point2.y);
+        console.log('=====view=====');
+        console.log(v.from.x, v.from.y, v.to.x, v.to.y);
+        console.log('=====additional=====');
+        console.log(a.fields[0].value, a.fields[1].value);
+    }
+    return function (arrowModel, arrowView, additional) {
+        additional.fields[0].change = function (v) {
+            arrowModel.point2.x = v;
+        };
+        additional.fields[1].change = function (v) {
+            arrowModel.point2.y = v;
+        };
+
+        function updateModel() {
+            arrowModel.point1.x = arrowView.from.x;
+            arrowModel.point1.y = arrowView.from.y;
+            arrowModel.point2.x = arrowView.to.x;
+            arrowModel.point2.y = arrowView.to.y;
+            updateAdditional();
+            logger(arrowModel, arrowView, additional);
+        }
+        function updateView() {
+            arrowView.from.x = arrowModel.point1.x;
+            arrowView.from.y = arrowModel.point1.y;
+            arrowView.to.x = arrowModel.point2.x;
+            arrowView.to.y = arrowModel.point2.y;
+            updateAdditional();
+            logger(arrowModel, arrowView, additional);
+            requestAnimationFrame(updateView);
+        }
+        function updateAdditional() {
+            additional.fields[0].value = arrowModel.point2.x;
+            additional.fields[1].value = arrowModel.point2.y;
+        }
+        updateView();
+    }
+});
