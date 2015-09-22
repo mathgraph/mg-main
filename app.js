0789cf5394a29694d6e7b1a98e7f3fc6503ff689
mf-side <mf-side@mail.ru>
2015-09-22 20:51:20 +0300

    fix(server): basic auth

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ var app = express();
 app
     .use(require('body-parser').urlencoded({ extended: false }))
     .use(require('body-parser').json())
-    .use(express.static(__dirname + '/static'))
     .use(function (req, res, next) {
         var credentials = auth(req);
         var true_auth = config.get((process.env.NODE_ENV == 'production') ? 'auth_prod' : 'auth_dev');
         if (!credentials || credentials.name !== true_auth.login || credentials.pass !== true_auth.password) {
             res.statusCode = 401;
-            res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+            res.setHeader('WWW-Authentic11ate', 'Basic realm="example"');
             res.end('Access denied')
         }
         else
             next();
-    });
+    })
+    .use(express.static(__dirname + '/static'));
 
 if (process.env.NODE_ENV == 'production'){
     app.listen(config.get('port_prod'), function(){
