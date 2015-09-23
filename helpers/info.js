5f03955ec7586902d6b20191ee8aba7f52dee7b1
sivsais <sivsais@gmail.com>
2015-09-23 21:57:45 +0300

    fix(info):

--- helpers/info.js
+++ helpers/info.js
@@ -1,25 +1,6 @@
 var express = require('express');
 var exec = require('child_process').exec;
 
-function syntaxHighlight(json) {
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
-
 module.exports = function (req, res, next) {
     exec('pm2 jlist', function (error, stdout, stderr) {
         var data = JSON.parse(stdout);
@@ -42,6 +23,6 @@ module.exports = function (req, res, next) {
             obj.memory = item.monit.memory;
             return obj;
         });
-        res.render('info', { text: syntaxHighlight(JSON.stringify(data, undefined, 4)) });
+        res.render('info', { data: data });
     });
 };
