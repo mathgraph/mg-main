7dd8a36790dd967c00e00d32969fe28cec049a70
sivsais <sivsais@gmail.com>
2015-10-05 13:26:01 +0300

    [59]fix(axesSwitcher): button

--- static/src/mathgraph/modules/axesSwitcher/switcher.js
+++ static/src/mathgraph/modules/axesSwitcher/switcher.js
@@ -1,11 +1,13 @@
-define(['lodash', 'jquery', '../../core/core'], function (_, $, core) {
+define(['lodash', '../../core/core'], function (_, core) {
 
-    core.extend('axesSwitcher', ['space2', 'axes', 'sheet'], function (module, space2, axes, sheet) {
-
-        $('#btn').click(function () {
-            axes.toggle();
-        });
-
+    core.extend('axesSwitcher', ['space2', 'axes', 'toolbar'], function (module, space2, axes, toolbar) {
+        module.toolbar({
+            type: 'button',
+            icon: 'http://dummyimage.com/50x50/ad6685/0c00f0.png&text=SW',
+            select: function () {
+                axes.toggle();
+            }
+        })
     });
 
 });
