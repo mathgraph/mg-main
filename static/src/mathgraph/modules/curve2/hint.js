e8b55e33ab79a6e37a94cacc6377860f0814d03c
sivsais <sivsais@gmail.com>
2015-10-16 06:08:12 +0300

    [86]feat(curve2+point): hints

--- static/src/mathgraph/modules/curve2/hint.js
+++ static/src/mathgraph/modules/curve2/hint.js
@@ -1,3 +1,11 @@
-/**
- * Created by sivsais on 11.10.15.
- */
+define(['../../core/core'], function (core) {
+    core.extend('Curve2', [], function (module) {
+        module.hint(function factory(model) {
+            return {
+                'a^2': model.axes.getCanonical().a2 || -1,
+                'b^2': model.axes.getCanonical().b2 || -1,
+                'p': model.axes.getCanonical().p || -1
+            }
+        });
+    });
+});
