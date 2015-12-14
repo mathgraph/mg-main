cf925ff31804fdc99b62cc2862698381266f0331
Maks <maks44ru@yandex.ru>
2015-12-14 18:55:20 +0300

    [96]feat(zoom)

--- static/src/mathgraph/modules/axesDrawer/axesDrawer.js
+++ static/src/mathgraph/modules/axesDrawer/axesDrawer.js
@@ -1,32 +1,45 @@
-define(['../../core/core'], function (core) {
+define(['../../core/core', 'lodash'], function (core, lodash) {
 
     core.extend('axesDrawer', ['space2', 'axes', 'sheet'], function (module, space2, axes, sheet) {
+        var recalcModel, recalcView, signOfRound, oldBasis, oldTypeAxes;
 
-        module.model(function () {
-            var ox = space2.make_line(),
-                oy = space2.make_line(),
-                or = space2.make_line();
-
-            var spOx = ox.make_project(sheet.axes),
-                spOy = oy.make_project(sheet.axes),
-                spOr = or.make_project(sheet.axes);
-
+        signOfRound = function (c) {
+            var n = -0;
+            if (Math.floor(c) === 0) {
+                do {
+                    n--;
+                    c *= 10;
+                } while (Math.floor(c) === 0)
+            } else {
+                c = Math.floor(c);
+                n = -1;
+                while (c !== 0) {
+                    n++;
+                    c = Math.floor(c / 10);
+                }
+            }
+            return n;
+        };
+        recalcModel = function (spOx, spOy, spOr) {
+            var basis = sheet.axes.basis,
+                min1 = Math.min(Math.abs(sheet.width / 2 / basis[0][0]), Math.abs(sheet.height / 2 / basis[0][1])),
+                min2 = Math.min(Math.abs(sheet.width / 2 / basis[1][0]), Math.abs(sheet.height / 2 / basis[1][1]));
             spOx.point1 = {
-                x: -sheet.width / 2,
-                y: 0
+                x: -basis[0][0] * min1,
+                y: -basis[0][1] * min1
             };
             spOx.point2 = {
-                x: sheet.width / 2,
-                y: 0
+                x: basis[0][0] * min1,
+                y: basis[0][1] * min1
             };
 
             spOy.point1 = {
-                x: 0,
-                y: sheet.height / 2
+                x: -basis[1][0] * min2,
+                y: -basis[1][1] * min2
             };
             spOy.point2 = {
-                x: 0,
-                y: -sheet.height / 2
+                x: basis[1][0] * min2,
+                y: basis[1][1] * min2
             };
 
             spOr.point1 = {
@@ -34,69 +47,120 @@ define(['../../core/core'], function (core) {
                 y: 0
             };
             spOr.point2 = {
-                x: sheet.width / 2,
+                x: basis[0][0] * min1,
                 y: 0
             };
-
+        };
+        recalcView = function () {
+            var basis = sheet.axes.basis,
+                maximumDistanceBetweenLabel = 60,
+                lengthX = Math.sqrt(basis[0][0] * basis[0][0] + basis[0][1] * basis[0][1]),
+                signOfRoundX = signOfRound(maximumDistanceBetweenLabel / lengthX),
+                lengthY = Math.sqrt(basis[1][0] * basis[1][0] + basis[1][1] * basis[1][1]),
+                signOfRoundY = signOfRound(maximumDistanceBetweenLabel / lengthY),
+                stepX = Math.floor(maximumDistanceBetweenLabel / lengthX * Math.pow(10, -signOfRoundX)) * Math.pow(10, signOfRoundX),
+                stepY = Math.floor(maximumDistanceBetweenLabel / lengthY * Math.pow(10, -signOfRoundY)) * Math.pow(10, signOfRoundY);
+            signOfRoundX = Math.max(0, -signOfRoundX);
+            signOfRoundY = Math.max(0, -signOfRoundY);
             return {
-                model: {
-                    ox: ox,
-                    oy: oy
+                paramsAX: {
+                    step: {
+                        length: stepX * lengthX,
+                        unit: 'px'
+                    },
+                    angle: -(Math.atan(basis[0][1] / basis[0][0]) + Math.atan(basis[1][0] / basis[1][1])) / Math.PI * 180
                 },
-                sheet: {
+                paramsAY: {
+                    step: {
+                        length: stepY * lengthY,
+                        unit: 'px'
+                    },
+                    angle: (Math.atan(basis[0][1] / basis[0][0]) + Math.atan(basis[1][0] / basis[1][1])) / Math.PI * 180
+                },
+                paramsP: {
+                    type: 'circles',
+                    start: 0,
+                    step: {
+                        length: stepX * lengthX,
+                        unit: 'px'
+                    }
+                },
+                contentX: function (o) {
+                    return (+o / lengthX).toFixed(signOfRoundX);
+                },
+                contentY: function (o) {
+                    return (+o / lengthY).toFixed(signOfRoundY);
+                },
+                contentP: function (o) {
+                    return (+o / lengthX).toFixed(signOfRoundX);
+                }
+            }
+        };
+
+        module.model(function () {
+            var spOx = {},
+                spOy = {},
+                spOr = {};
+            recalcModel(spOx, spOy, spOr);
+
+            oldBasis = _.cloneDeep(sheet.axes.basis);
+            oldTypeAxes = axes.get().type;
+            return {
                     ox: spOx,
                     oy: spOy,
                     or: spOr
-                }
-
             }
         });
 
         var last = axes.get().type;
 
         module.view('axes', function factory(model) {
-            var paramsA = {
-                step: {
-                    length: '50',
-                    unit: 'px'
-                }
-            }, paramsP = {
-                type: 'circles',
-                start: 0,
-                step: {
-                    length: '50',
-                    unit: 'px'
-                }
-            };
-            var content = function (o) {
-                return (+o / 10).toFixed(0);
-            };
+            var config = recalcView();
             return {
-                ox: sheet.draw_arrow(model.sheet.ox.point1, model.sheet.ox.point2, {
-                        strokeColor: 'black',
-                        strokeWidth: 1
-                    }).ticker(paramsA).grid(paramsA).labeled(content, paramsA).set('interactive', false),
-                oy: sheet.draw_arrow(model.sheet.oy.point1, model.sheet.oy.point2, {
-                        strokeColor: 'black',
-                        strokeWidth: 1
-                    }).ticker(paramsA).grid(paramsA).labeled(content, paramsA).set('interactive', false),
-                or: sheet.draw_arrow(model.sheet.or.point1, model.sheet.or.point2, {
-                        strokeColor: 'black',
-                        strokeWidth: 1
-                    }).ticker(paramsP).grid(paramsP).labeled(content, paramsP).set('interactive', false).hide()
+                ox: sheet.draw_arrow(model.ox.point1, model.ox.point2, {
+                    strokeColor: 'black',
+                    strokeWidth: 1
+                }).ticker(config.paramsAX).grid(config.paramsAX).labeled(config.contentX, config.paramsAX).set('interactive', false),
+                oy: sheet.draw_arrow(model.oy.point1, model.oy.point2, {
+                    strokeColor: 'black',
+                    strokeWidth: 1
+                }).ticker(config.paramsAY).grid(config.paramsAY).labeled(config.contentY, config.paramsAY).set('interactive', false),
+                or: sheet.draw_arrow(model.or.point1, model.or.point2, {
+                    strokeColor: 'black',
+                    strokeWidth: 1
+                }).ticker(config.paramsP).grid(config.paramsP).labeled(config.contentP, config.paramsP).set('interactive', false).hide()
             }
         }, function update(model, view) {
-            switch (axes.get().type) {
-                case 'affine':
-                    view.ox.show();
-                    view.oy.show();
-                    view.or.hide();
-                    break;
-                case 'polar':
-                    view.ox.hide();
-                    view.oy.hide();
-                    view.or.show();
-                    break;
+            var basis = sheet.axes.basis;
+            if (!_.eq(oldBasis, basis) || oldTypeAxes !== axes.get().type) {
+                var config = recalcView();
+                recalcModel(model.ox, model.oy, model.or);
+                switch (axes.get().type) {
+                    case 'affine':
+                        view.ox.show();
+                        view.oy.show();
+                        view.or.hide();
+
+                        view.ox.from = model.ox.point1;
+                        view.ox.to = model.ox.point2;
+                        view.oy.from = model.oy.point1;
+                        view.oy.to = model.oy.point2;
+                        view.ox.ticker(config.paramsAX).grid(config.paramsAX).labeled(config.contentX, config.paramsAX);
+                        view.oy.ticker(config.paramsAY).grid(config.paramsAY).labeled(config.contentX, config.paramsAY);
+
+                        break;
+                    case 'polar':
+                        view.ox.hide();
+                        view.oy.hide();
+                        view.or.show();
+
+                        view.or.from = model.or.point1;
+                        view.or.to = model.or.point2;
+                        view.or.ticker(config.paramsP).grid(config.paramsP).labeled(config.contentX, config.paramsP);
+                        break;
+                }
+                oldBasis =  _.cloneDeep(basis);
+                oldTypeAxes = axes.get().type;
             }
         });
 
@@ -107,6 +171,8 @@ define(['../../core/core'], function (core) {
                 .show('axes');
         };
 
-    });
+    })
+    ;
 
-});
+})
+;
--- static/src/mathgraph/primary/sheet.js
+++ static/src/mathgraph/primary/sheet.js
@@ -1,10 +1,28 @@
-define(['../core/core', 'mg-sheet'], function (core, Sheet) {
+define(['../core/core', 'mg-sheet', 'jquery'], function (core, Sheet, $) {
 
     core.module('sheet', ['space2'], function (moduleProto, space2) {
         var sheet = new Sheet('canvas');
 
         sheet.axes = space2.make_axes('affine');
-        sheet.axes.basis = [[10, 0], [0, -10]];
+        sheet.axes.basis = [[50, 0], [0, -50]];
+
+        sheet.on('wheel', function (e) {
+            var d = 0.1,
+                basis = sheet.axes.basis;
+            if (e.wheelDelta > 0) {
+                d = 1 + d;
+            } else {
+                d = 1 - d;
+            }
+            basis[0][0] *= d;
+            basis[0][1] *= d;
+            basis[1][0] *= d;
+            basis[1][1] *= d;
+        });
+
+        $(sheet.domElem).on('wheel', function (e) {
+
+        });
 
         moduleProto.tool = function (name, desc) {
             if (typeof name !== 'string') {
